1a82b728c6bb2f4759a492c121f722919da9a172
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-05-22 23:39:13 +0700

    connect to mongodb atlas

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const compression_1 = __importDefault(require("compression"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    credentials: true,
+}));
+app.use((0, compression_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use(body_parser_1.default.json());
+const server = http_1.default.createServer(app);
+server.listen(8080, () => {
+    console.log('Server running on port: 8080');
+});
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -19,4 +20,12 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
     console.log('Server running on port: 8080');
+});
+
+
+const MONGO_URL = 'mongodb+srv://thanhtruong1901:uit123456@cluster0.jfn8nad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) =>{
+    console.log(error);
 })
