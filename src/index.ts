b990aabdfd2309c6ecdb3da0bcb5aba8c60ad7e1
ThanhhTruongg1901 <thanhtruong1901@gmail.com>
2024-05-23 23:32:24 +0700

    authentication routing and logging in

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import router from './routers/router';
+
 const app = express();
 
 app.use(cors({
@@ -29,3 +31,5 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) =>{
     console.log(error);
 })
+
+app.use('/', router());
